879de752ec93e7950ad6dabb0ed29de48dbd8a5f
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-02-01 23:58:12 +0100

    last update in second test

--- src/test/java/com/vytrack/pages/CalendarEventsPage.java
+++ src/test/java/com/vytrack/pages/CalendarEventsPage.java
@@ -3,9 +3,12 @@ package com.vytrack.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class CalendarEventsPage extends BasePage{
+public class CalendarEventsPage extends BasePage {
 
     @FindBy(xpath = "//div[normalize-space()='Options']")
     public WebElement options;
+
+    @FindBy(css = "input[type='number']")
+    public WebElement pageNumber;
 
 }
--- src/test/java/com/vytrack/tests/VytrackHomeworkClass.java
+++ src/test/java/com/vytrack/tests/VytrackHomeworkClass.java
@@ -5,13 +5,14 @@ import com.vytrack.pages.DashboardPage;
 import com.vytrack.pages.LoginPage;
 import com.vytrack.utilities.ConfigurationReader;
 import org.testng.Assert;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 public class VytrackHomeworkClass extends TestBase {
 
 
     @Test
-    public void optionsIsDisplayed(){
+    public void optionsIsDisplayed() {
         extentLogger = report.createTest("Options is displayed Test");
 
         LoginPage loginPage = new LoginPage();
@@ -26,13 +27,31 @@ public class VytrackHomeworkClass extends TestBase {
         new DashboardPage().navigateToModule("Activities", "Calendar Events");
 
         extentLogger.info("Verify Options is Displayed");
-        Assert.assertTrue(new CalendarEventsPage().options.isDisplayed(),"verify options is displayed");
+        Assert.assertTrue(new CalendarEventsPage().options.isDisplayed(), "verify options is displayed");
 
         extentLogger.pass("PASS: Options is displayed test");
-
-
-
     }
 
+    @Test
+    public void pageNumberTest() {
+        extentLogger = report.createTest("Page Number is equals to 1 Test");
 
+        LoginPage loginPage = new LoginPage();
+
+        extentLogger.info("username : " + ConfigurationReader.get("storemanager_username"));
+        extentLogger.info("password : " + ConfigurationReader.get("storemanager_password"));
+        extentLogger.info("login as a store manager");
+        loginPage.loginAsStoreManager();
+
+        extentLogger.info("go to the Activities ---> Calendar Events");
+        new DashboardPage().waitUntilLoaderScreenDisappear();
+        new DashboardPage().navigateToModule("Activities", "Calendar Events");
+
+        extentLogger.info("Verify the page number is 1");
+        new DashboardPage().waitUntilLoaderScreenDisappear();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        assertEquals(calendarEventsPage.pageNumber.getAttribute("value"), "2", "verify the page number");
+        extentLogger.pass("PASS: Page Number is equals to 1 Test");
+
+    }
 }
